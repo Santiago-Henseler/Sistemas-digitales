6c68018f170ebcb0ce8c00f4a8b7f55671479144
Santy <shenseler@fi.uba.ar>
2024-10-04 15:40:03 -0300

    boceto multiplicador

--- Tp2/multiplicador_float.vhdl
+++ Tp2/multiplicador_float.vhdl
@@ -19,24 +19,34 @@ end multiplicador_float;
 
 architecture multiplicador_float_arch of multiplicador_float is
 
+    constant SIZE: natural := Ne+Nf;
     signal bias: integer := 2**(Ne-1)-1;
-
+    
     signal significant_mult: unsigned(2*Nf-1 downto 0);
     signal exp, expA, expB: unsigned(Ne-1 downto 0);
-    signal mantisaA, mantisaB: unsigned(Nf-1 downto 0);
+    signal significantA, significantB: unsigned(Nf-1 downto 0);
     signal signo: std_logic;
     
 begin
 
-    signo <= operandoA(Ne+Nf-1) xor operandoB(Ne+Nf-1);
-    expA <= unsigned(operandoA(Ne+Nf-2 downto Nf-1));
-    expB <= unsigned(operandoB(Ne+Nf-2 downto Nf-1));
-    exp <= expA + expB - to_unsigned(bias, Ne-1);
+    signo <= operandoA(SIZE-1) xor operandoB(SIZE-1);
+    expA <= unsigned(operandoA(SIZE-2 downto Nf-1));
+    expB <= unsigned(operandoB(SIZE-2 downto Nf-1));
+    
+    significantA <= '1' & unsigned(operandoA(Nf-2 downto 0));
+    significantB <= '1' & unsigned(operandoB(Nf-2 downto 0));
+    significant_mult <= significantA * significantB;
 
-    mantisaA <= '1' & unsigned(operandoA(Nf-2 downto 0));
-    mantisaB <= '1' & unsigned(operandoB(Nf-2 downto 0));
-
-    significant_mult <= mantisaA * mantisaB;
+    process(significant_mult)
+    begin
+        if significant_mult(2*Nf-1) = '0' then 
+            --while significant_mult(2*Nf-1) = '0' then
+            --  Desplazo un a posicion significant_mult 
+            --end while;
+        else
+            exp <= expA + expB - to_unsigned(bias, Ne-1) + to_unsigned(1, Ne-1);
+        end if;
+    end process; 
 
     resultado <= signo & std_logic_vector(exp & significant_mult(2*Nf-2 downto Nf));
 
