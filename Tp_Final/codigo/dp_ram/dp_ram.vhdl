7f74c7e4a8e903baf4b6ef300233460882916869
Santy <shenseler@fi.uba.ar>
2024-12-30 12:01:03 -0300

    cambios en dpram

--- Tp_Final/codigo/dp_ram/dp_ram.vhdl
+++ Tp_Final/codigo/dp_ram/dp_ram.vhdl
@@ -4,17 +4,15 @@ use IEEE.numeric_std.all;
 
 entity dual_ram is
     generic(
-		ADD_W: natural := 20  -- uso direcciones de 20 bits porque log(307199) es 19,585 
+		ADD_W: natural := 20
     );
     port(
-        write1: in std_logic;
-        read1: in std_logic;
-        read2: in std_logic;
-        addr1: in std_logic_vector(ADD_W-1 downto 0); 
-        addr2: in std_logic_vector(ADD_W-1 downto 0);
-        data1_i: in std_logic;
-        data1_o: out std_logic;
-        data2_o: out std_logic
+        write: in std_logic;
+        read: in std_logic;
+        addrW: in std_logic_vector(ADD_W-1 downto 0); 
+        addrR: in std_logic_vector(ADD_W-1 downto 0);
+        data_i: in std_logic;
+        data_o: out std_logic;
     );
 end dual_ram;
 
@@ -22,23 +20,19 @@ architecture dual_ram_arch of dual_ram is
     type ram_type is array (0 to 2**(ADD_W)-1) of std_logic;
     shared variable RAM : ram_type :=(others => '0');
 begin
-    process(write1, read1, addr1)
+    process(write, addrW)
     begin
-        if write1 = '1' and read1 = '0' then
-           RAM(to_integer(unsigned(addr1))) := data1_i;
-        elsif write1 = '0' and read1 = '1' then
-            data1_o <= RAM(to_integer(unsigned(addr1)));
-        else
-            data1_o <= '0';
+        if write = '1' then
+           RAM(to_integer(unsigned(addrW))) := data_i;
         end if;
     end process;
 
-    process(read2, addr2)
+    process(read, addrR)
     begin
-        if read2 = '1' then
-            data2_o <= RAM(to_integer(unsigned(addr2)));
+        if read = '1' then
+            data_o <= RAM(to_integer(unsigned(addrR)));
         else
-            data2_o <= '0';
+            data_o <= '0';
         end if;
     end process;
 end dual_ram_arch;
